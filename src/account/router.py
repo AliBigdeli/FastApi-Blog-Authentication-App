a7e0a35aa4d088e38758df8c91cc77bc4596db57
TheAliBigdeli <bigdeli.ali3@gmail.com>
2022-11-28 13:08:55 +0330

    added refresh token to account router

--- src/account/router.py
+++ src/account/router.py
@@ -12,6 +12,10 @@ router = APIRouter(
 def account_login(username,password):
     return {"access_token":"adasdasdasdasd","refresh_token":"asdasdasdasd","type":"Bearer"}
 
+@router.post('/refresh/')
+def account_refresh_token(refresh_token):
+    return {"access_token":"adasdasdasdasd"}
+
 @router.post('/register/')
 def account_register(username,password):
     return {"username": username, "password": password}
