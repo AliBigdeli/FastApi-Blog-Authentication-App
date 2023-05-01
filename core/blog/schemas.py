a54bf25103e3a92545ab9a455ab522398df2ea73
TheAliBigdeli <bigdeli.ali3@gmail.com>
2023-05-01 07:19:11 +0330

    Update schemas.py

--- core/blog/schemas.py
+++ core/blog/schemas.py
@@ -4,10 +4,12 @@ from datetime import datetime
 class PostSchema(BaseModel):
     title : str
     content : str
+    is_published: bool
     
 class PostUpdateSchema(BaseModel):
     title : Optional[str]= None
     content : Optional[str]= None
+    is_published: Optional[str]= None
     
 
 class PostResponse(BaseModel):
@@ -17,6 +19,19 @@ class PostResponse(BaseModel):
     content: str
     created_at: Optional[datetime]
     modified_at: Optional[datetime]
+
+    class Config:
+        orm_mode = True
+        
+
+class AuthorPostResponse(BaseModel):
+    id: Optional[int]
+    title: str
+    user: int
+    content: str
+    created_at: Optional[datetime]
+    modified_at: Optional[datetime]
+    is_published: bool
 
     class Config:
         orm_mode = True
